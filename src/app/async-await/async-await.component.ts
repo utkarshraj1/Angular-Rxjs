c1cd8a840ad1732df24d233ec9b8783b72f6a360
Utkarsh Raj <utkarsh.raj@outlook.com>
2021-10-29 17:45:34 +0530

    component making

---
+++ src/app/async-await/async-await.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-async-await',
+  templateUrl: './async-await.component.html',
+  styleUrls: ['./async-await.component.scss']
+})
+export class AsyncAwaitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
