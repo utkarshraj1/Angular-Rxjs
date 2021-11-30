c70c0fc5873bc8ac799ba61830e46921bf039f68
Utkarsh Raj <utkraj@publicisgroupe.net>
2021-11-30 22:54:44 +0530

    Custom observables starter files

---
+++ src/app/observables/custom-observable/custom-observable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-observable',
+  templateUrl: './custom-observable.component.html',
+  styleUrls: ['./custom-observable.component.scss']
+})
+export class CustomObservableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
