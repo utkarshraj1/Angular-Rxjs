127ec5836bc2675b27203ac8f4436ac65409c82b
Utkarsh Raj <utkraj@publicisgroupe.net>
2021-11-30 12:52:45 +0530

    toArray implementation start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { IntervalComponent } from './observables/interval/interval.component';
 import { ListComponent } from './observables/list/list.component';
 import { ObservablesComponent } from './observables/observables.component';
 import { OfFromComponent } from './observables/of-from/of-from.component';
+import { ToArrayComponent } from './observables/to-array/to-array.component';
 import { PromiseComponent } from './promise/promise.component';
 
 const routes: Routes = [
@@ -17,7 +18,8 @@ const routes: Routes = [
       { path: '', component: ListComponent },
       { path: 'from-event', component: FromEventComponent },
       { path: 'interval', component: IntervalComponent },
-      { path: 'of-from', component: OfFromComponent }
+      { path: 'of-from', component: OfFromComponent },
+      { path: 'to-array', component: ToArrayComponent }
     ],
   },
   { path: 'asyncAwait', component: AsyncAwaitComponent },
