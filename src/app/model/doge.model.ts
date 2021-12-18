86b91ad4d4a82afd2891e752905c6700fa6116e7
Utkarsh Raj <utkraj@publicisgroupe.net>
2021-12-18 23:08:39 +0530

    Pluck operator implemented

---
+++ src/app/model/doge.model.ts
@@ -0,0 +1,9 @@
+export interface IDoge {
+    author: string;
+    created: string;
+    permalink: string;
+    submission_id: string;
+    submission_title: string;
+    submission_url: string;
+    timestamp: string;
+}
--- src/app/observables/pluck/pluck.component.ts
+++ src/app/observables/pluck/pluck.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+import { IDoge } from 'src/app/model/doge.model';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-pluck',
   templateUrl: './pluck.component.html',
   styleUrls: ['./pluck.component.scss']
 })
-export class PluckComponent implements OnInit {
+export class PluckComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _url: string;
+  memeData: IDoge[];
+  memeCount!: number;
+
+  getDataSub!: Subscription;
+
+  constructor(private shared: SharedService) {
+    this._url = 'https://api.doge-meme.lol/v1/memes/?skip=0&limit=';
+    this.memeData = [];
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    if (this.getDataSub !== undefined) {
+      this.getDataSub.unsubscribe();
+    }
+  }
+
+  searchMeme(): void {
+    const customUrl = this._url + this.memeCount;
+    this.getDataSub = this.shared.getData(customUrl).pipe(pluck('data')).subscribe(res => {
+      // console.log(res);
+      this.memeData = res;
+    });
+  }
 }
