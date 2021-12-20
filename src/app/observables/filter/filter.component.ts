8c367542f853a28284160be31d52a591f7a3ddf9
Utkarsh Raj <utkraj@publicisgroupe.net>
2021-12-20 22:17:18 +0530

    filter implementation done

--- src/app/observables/filter/filter.component.ts
+++ src/app/observables/filter/filter.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { from, Subscription } from 'rxjs';
+import { filter, toArray } from 'rxjs/operators';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _url: string;
+  photosData: any;
+  dataObj: { max: number, min: number };
 
-  ngOnInit(): void {
+  subsHandler!: Subscription;
+  fromSub!: Subscription;
+
+  constructor(private shared: SharedService) {
+    this._url = 'https://jsonplaceholder.typicode.com/photos';
+    this.photosData = [];
+    this.dataObj = { min: 0, max: 0 };
   }
 
+  ngOnInit(): void {
+    // this.shared.getData(this._url).subscribe(res => {
+    //   console.log(res);
+    // });
+    // this.getPhotosFromAPI();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subsHandler !== undefined) { this.subsHandler.unsubscribe(); }
+    if (this.fromSub !== undefined) { this.fromSub.unsubscribe(); }
+  }
+
+  getPhotosFromAPI(): void {
+    this.subsHandler = this.shared.getData(this._url).subscribe(res => {
+      // console.log(res);
+      this.filterDataCustom(res);
+    });
+  }
+
+  filterDataCustom(res: []): void {
+    const data = from(res);
+    this.fromSub = data.pipe(filter((value: any) => value.albumId >= this.dataObj.min && value.albumId <= this.dataObj.max), toArray())
+      .subscribe(res => {
+        // console.log(res);
+        this.photosData = res;
+      });
+  }
 }
