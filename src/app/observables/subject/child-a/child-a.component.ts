65f0262dae0ff0a2021b2ad0c48ca0d7efa3a10c
Utkarsh Raj <utkraj@publicisgroupe.net>
2021-12-27 16:59:56 +0530

    subjects implemented

--- src/app/observables/subject/child-a/child-a.component.ts
+++ src/app/observables/subject/child-a/child-a.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectsService } from 'src/app/services/subjects/subjects.service';
 
 @Component({
   selector: 'app-child-a',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildAComponent implements OnInit {
 
-  constructor() { }
+  productType: string;
+  productTypeData: Array<string> = [
+    'Blush',
+    'Bronzer',
+    'Eyebrow',
+    'Eyeliner',
+    'Eyeshadow',
+    'Foundation'
+  ];
+  loadingStatus!: string;
+
+  constructor(private sub: SubjectsService) {
+    this.productType = 'Select here';
+  }
 
   ngOnInit(): void {
+    this.sub.loadStatus.subscribe(res => {
+      this.loadingStatus = res;
+    });
+  }
+
+  getProduct(): void {
+    // console.log(this.productType);
+    this.sub.inputData.next(this.productType);
   }
 
 }
--- src/app/observables/subject/child-b/child-b.component.ts
+++ src/app/observables/subject/child-b/child-b.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/services/shared/shared.service';
+import { SubjectsService } from 'src/app/services/subjects/subjects.service';
 
 @Component({
   selector: 'app-child-b',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildBComponent implements OnInit {
 
-  constructor() { }
+  private _url: string;
 
-  ngOnInit(): void {
+  productType!: Array<any>;
+
+  pResSub!: Subscription;
+
+  constructor(private sub: SubjectsService, private shared: SharedService) {
+    this._url = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=';
   }
 
+  ngOnInit(): void {
+    this.getProductsTypeJSON();
+  }
+
+  getProductsTypeJSON(): void {
+    this.sub.inputData.subscribe(res => {
+      // console.log("In child b: ", res);
+      this.sub.loadStatus.next('Loading...');
+      const cUrl = this._url + res;
+      this.pResSub = this.shared.getData(cUrl).subscribe(prod => {
+        // console.log(prod);
+        this.productType = prod;
+        this.sub.loadStatus.next('Loaded.');
+      });
+    });
+  }
 }
